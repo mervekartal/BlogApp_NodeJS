5f29c1871cf64270f27b49865c350545a427159a
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-16 15:35:22 +0300

    views folder and ejs files added

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
+app.use(express.static('public'))
+app.set("view engine", "ejs")
+
 
 app.get('/', (req, res) => {
-    
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+    res.render('index')
+  })
+app.get('/about', (req, res) => {
+    res.render('about')
+  })
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
   })
   
   const port = 3000;
