c55a1540e757fc896d8b62da60777888bd6b44ed
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-21 17:42:43 +0300

    db cruds operations

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const ejs = require('ejs')
+
+const Blog = require('./models/Blog')
 
 const app = express()
 app.use(express.static('public'))
 app.set("view engine", "ejs")
 
 
-app.get('/', (req, res) => {
-    res.render('index')
+//connect db
+mongoose.connect('mongodb://localhost:27017/cleanblog-test-db')
+
+process.on('warning', (warning) => {
+    console.log(warning.stack);
+})
+
+
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.render('index', {
+  blogs
   })
+})
+
 app.get('/about', (req, res) => {
     res.render('about')
-  })
+})
 app.get('/add_post', (req, res) => {
     res.render('add_post')
-  })
+})
   
+
+  //add blog content - post operation
+  app.post('/blog', async (req, res) => {
+    await Blog.create(req.body)
+    res.redirect('/')
+  })
+
+
   const port = 3000;
   app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı`);
---
+++ models/Blog.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema 
+
+
+//create schema
+const BlogSchema = new Schema({
+    title: String,
+    detail: String,
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Blog = mongoose.model('Blog',BlogSchema)
+
+module.exports = Blog
+
