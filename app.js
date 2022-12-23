40eca41994d4cbc356b1d8544efcc83a2862874d
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-23 09:25:01 +0300

    update and delete

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const ejs = require('ejs')
+const methodOverride = require('method-override')
+
 
 const Post = require('./models/Post')
 
+//middlewares
 const app = express()
 app.use(express.static('public'))
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET']
+}))
+
 
 //connect db
 mongoose.connect('mongodb://localhost:27017/cleanblog-test-db')
@@ -45,18 +52,39 @@ app.get('/add_post', (req, res) => {
 })
   
 
-  //add blog post - post operation
-  app.post('/posts', async (req, res) => {
-    // console.log(req.body)
-    await Post.create(req.body)
-    res.redirect('/')
-  })
+//add blog post - post operation
+app.post('/posts', async (req, res) => {
+  // console.log(req.body)
+  await Post.create(req.body)
+  res.redirect('/')
+})
 
+//update routing
+app.get('/posts/edit/:id', async (req, res) => {
+  const post = await Post.findOne({_id: req.params.id})
+  res.render('edit', {post})
+})
 
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı`);
-  });
+//update post
+app.put('/posts/:id', async (req, res) => {
+  const post = await Post.findOne({_id: req.params.id})
+  post.title = req.body.title
+  post.detail = req.body.detail
+  post.save()
+
+  res.redirect(`/posts/${req.params.id}`)
+})
+
+//delete post
+app.delete('/posts/:id', async (req, res) => {
+  await Post.findByIdAndRemove(req.params.id)
+  res.redirect('/')
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
   
 
 
